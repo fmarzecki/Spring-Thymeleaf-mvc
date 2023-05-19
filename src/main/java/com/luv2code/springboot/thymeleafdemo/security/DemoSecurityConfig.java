f4644c38c19b8908179aae6bfa1ea1790e008c69
fmarzecki <filipo085@icloud.com>
2023-05-19 23:46:19 +0200

    JDBC Authentication With Custom Table

--- src/main/java/com/luv2code/springboot/thymeleafdemo/security/DemoSecurityConfig.java
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/security/DemoSecurityConfig.java
@@ -1,5 +1,7 @@
 package com.luv2code.springboot.thymeleafdemo.security;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,34 +9,49 @@ import org.springframework.security.config.annotation.web.configurers.LogoutConf
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class DemoSecurityConfig {
 
-    @Bean 
-    public InMemoryUserDetailsManager userDetailsManager() {
+    @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+        JdbcUserDetailsManager theUserDetailsManager = new JdbcUserDetailsManager(dataSource);
 
-        UserDetails john = User.builder()
-            .username("john")
-            .password("{noop}test123")
-            .roles("EMPLOYEE")
-            .build();
+        theUserDetailsManager
+            .setUsersByUsernameQuery("select user_id, pw, active from members where user_id=?");
 
-        UserDetails mary = User.builder()
-            .username("mary")
-            .password("{noop}test123")
-            .roles("EMPLOYEE", "MANAGER")
-            .build();
+        theUserDetailsManager
+            .setAuthoritiesByUsernameQuery("select user_id, role from roles where user_id=?");
 
-        UserDetails susan = User.builder()
-            .username("susan")
-            .password("{noop}test123")
-            .roles("EMPLOYEE", "MANAGER", "ADMIN")
-            .build();
-
-        return new InMemoryUserDetailsManager(john, mary, susan);
+        return theUserDetailsManager;
     }
+
+    // @Bean 
+    // public InMemoryUserDetailsManager userDetailsManager() {
+
+    //     UserDetails john = User.builder()
+    //         .username("john")
+    //         .password("{noop}test123")
+    //         .roles("EMPLOYEE")
+    //         .build();
+
+    //     UserDetails mary = User.builder()
+    //         .username("mary")
+    //         .password("{noop}test123")
+    //         .roles("EMPLOYEE", "MANAGER")
+    //         .build();
+
+    //     UserDetails susan = User.builder()
+    //         .username("susan")
+    //         .password("{noop}test123")
+    //         .roles("EMPLOYEE", "MANAGER", "ADMIN")
+    //         .build();
+
+    //     return new InMemoryUserDetailsManager(john, mary, susan);
+    // }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
